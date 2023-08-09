4fddba551eb62ea69ea4bd3fb02113aa60d12bb7
miguel.tinembart <miguel.tinembart@gmail.com>
2023-08-10 00:11:02 +0200

    fix: let migrate throw error as this step is needed every step for now

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
-import { buildServer } from 'utils/server';
-import { env } from 'config/env';
-
+import { buildServer } from './utils/server';
+import { env } from './config/env';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { db } from 'utils/db';
+import { db } from './utils/db';
+import { DatabaseError } from 'pg';
 
 async function main() {
-  const app = await buildServer();
-
   //Register swagger
-
+  const app = await buildServer();
   // Migrate db for changes
 
-  await migrate(db, { migrationsFolder: env.MIGRATION_FOLDER })
-    .catch((e) => {
-      if (!e) {
-        app.log.info("success")
-      }
-      app.log.error(e, 'Migration failed');
-    })
+  await migrate(db, { migrationsFolder: env.MIGRATION_FOLDER });
 
   app.listen({
     port: env.PORT,
