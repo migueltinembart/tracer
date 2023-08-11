a726e9c3184cf0f76c1aa16714eabad86ebed126
miguel.tinembart <miguel.tinembart@rebsamen.net>
2023-08-11 17:37:54 +0200

    feat: added swagger support

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -2,6 +2,10 @@ import fastify from 'fastify';
 import { logger } from './logger';
 import { env } from '../config/env';
 import sitesRoutes from 'modules/sites/sites.routes';
+import fp from "fastify-plugin"
+import healthRoutes from 'modules/health/health.routes';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
 
 export async function buildServer() {
   const app = fastify({
@@ -14,9 +18,27 @@ export async function buildServer() {
     await app.close();
   }
 
-  app.register(sitesRoutes, {prefix: "api/sites"})
-
   const signals = ['SIGINT', 'SIGTERM'];
+
+  app.register(fastifySwagger, {
+    swagger: {
+      info: {
+        title: 'tracer API',
+        description: 'test',
+        version: '3.0.0',
+      },
+      host: 'localhost',
+      schemes: ['http'],
+      externalDocs: {
+        url: 'https://rebsamen.net',
+        description: 'Test it out somewhere else',
+      },
+      consumes: ['application/json'],
+      produces: ['application/json'],
+    },
+  });
+
+  app.register(fastifySwaggerUi, { prefix: 'docs' });
 
   for (const signal of signals) {
     process.on(signal, () => {
@@ -25,6 +47,12 @@ export async function buildServer() {
       });
     });
   }
+
+  app.register(healthRoutes, { prefix: 'api/health' });
+  app.register(sitesRoutes, { prefix: 'api/sites' });
+
+  await app.ready();
+  app.swagger();
 
   return app;
 }
