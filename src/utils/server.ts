ed98cd5a4c9dcb8fd4dbd72674eaaae91d15ae8e
miguel.tinembart <miguel.tinembart@gmail.com>
2023-08-17 08:33:45 +0200

    feat: registered new routes
    - changed health to status
    - added siteGroupsRoutes
    - added tenantroutes
    - added tenantGroupsRoutes

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,10 +1,13 @@
 import fastify from 'fastify';
 import { logger } from './logger';
 import { env } from '../config/env';
-import healthRoutes from 'modules/health/health.routes';
+import healthRoutes from 'modules/health/routes';
 import fastifySwagger from '@fastify/swagger';
 import fastifySwaggerUi from '@fastify/swagger-ui';
 import sitesRoutes from 'modules/sites/routes';
+import siteGroupsRoutes from 'modules/siteGroups/routes';
+import tenantsRoutes from 'modules/tenants/routes';
+import tenantGroupsRoutes from 'modules/tenantGroups/routes';
 
 export async function buildServer() {
   const app = fastify({
@@ -25,8 +28,11 @@ export async function buildServer() {
   await app.register(fastifySwaggerUi, { routePrefix: 'docs' });
 
   // Register routes here
-  await app.register(healthRoutes, { prefix: 'api/health' });
+  await app.register(healthRoutes, { prefix: 'api/status' });
   await app.register(sitesRoutes, { prefix: 'api/sites' });
+  await app.register(siteGroupsRoutes, { prefix: '/api/site-groups' });
+  await app.register(tenantsRoutes, { prefix: 'api/tenants' });
+  await app.register(tenantGroupsRoutes, { prefix: 'api/tenant-groups' });
 
   await app.ready();
   app.swagger();
