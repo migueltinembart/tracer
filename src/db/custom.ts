3105d7ae9f458046e14c3d43feb4b915b0441e8f
miguel.tinembart <miguel.tinembart@gmail.com>
2023-08-17 08:30:28 +0200

    change: changed from uuid to serial

--- src/db/custom.ts
+++ src/db/custom.ts
@@ -1,10 +1,10 @@
-import { primaryKey, text, timestamp, uniqueIndex, uuid, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { primaryKey, serial, text, timestamp, uniqueIndex, uuid, pgTable, varchar } from 'drizzle-orm/pg-core';
 
 export const tags = pgTable(
   'tags',
   {
-    id: uuid('id').defaultRandom().notNull(),
-    name: varchar("name"),
+    id: serial('id').notNull(),
+    name: varchar('name'),
     updatedAt: timestamp('updated_at').defaultNow().notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
   },
