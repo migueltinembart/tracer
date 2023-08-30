35e6969cd7cb1d217995ede07037a4fa64688ef3
miguel.tinembart <miguel.tinembart@gmail.com>
2023-08-31 00:25:58 +0200

    dashboard Items created
    - can be freely placed with col start
    - will get customized forms later

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
-import { SiteCreatorCard } from "./components/layout/dashboardItems.tsx/SIteCreatorCard";
+import { SiteCreatorCard, TenantCreatorCard } from "@/components/layout/dashboardItems";
 import { NavBar } from "./components/layout/navbar";
-
 
 function App() {
   return (
@@ -8,8 +7,9 @@ function App() {
       <div className="w-full h-full flex flex-col pb-6">
         <NavBar className="h-12 border-b-[2px] border-gray-300 shadow-md flex items-center gap-8 py-2 mb-6 px-28"></NavBar>
         <div className="w-full h-full px-28">
-          <div className="border border-gray-300 rounded-sm grid h-full p-6 grid-cols-5 grid-rows-5 gap-6 grid-flow-col">
+          <div className="border border-gray-300 rounded-sm grid h-full p-6 lg:grid-cols-6 grid-rows-5 md:grid-cols-4 gap-6 grid-flow-row">
             <SiteCreatorCard></SiteCreatorCard>
+            <TenantCreatorCard className="row-start-3"></TenantCreatorCard>
           </div>
         </div>
       </div>
