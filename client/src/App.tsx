222e56d608119a14db3a43ca26c0285825ebfbe5
miguel.tinembart <miguel.tinembart@gmail.com>
2023-08-30 12:50:16 +0200

    shadcn initialized correctly with pnpm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,69 +10,79 @@ function App() {
         <div className="drawer lg:drawer-open">
           <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content ">
-            <div className="navbar h-16 sticky top-0 bg-base-100">
-              <div className="flex-none">
-                <div className="dropdown dropdown-end">
-                  <label tabIndex={0} className="btn btn-ghost btn-circle">
-                    <div className="indicator">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                        />
-                      </svg>
-                      <span className="badge badge-sm indicator-item">8</span>
-                    </div>
-                  </label>
-                  <div
-                    tabIndex={0}
-                    className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
-                  >
-                    <div className="card-body">
-                      <span className="font-bold text-lg">8 Items</span>
-                      <span className="text-info">Subtotal: $999</span>
-                      <div className="card-actions">
-                        <button className="btn btn-primary btn-block">
-                          View cart
-                        </button>
+            <div className="navbar h-16 max-md:sticky top-0 bg-base-100">
+              <div className="navbar-start">
+                <div className="h-16 px-4 flex items-cente lg:hidden">
+                  <a className="normal-case text-xl p-4">Tracer</a>
+                </div>
+              </div>
+              <div className="navbar-center">
+                
+              </div>
+              <div className="navbar-end">
+                <div className="flex">
+                  <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                      <div className="indicator">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                          />
+                        </svg>
+                        <span className="badge badge-sm indicator-item">8</span>
+                      </div>
+                    </label>
+                    <div
+                      tabIndex={0}
+                      className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow"
+                    >
+                      <div className="card-body">
+                        <span className="font-bold text-lg">8 Items</span>
+                        <span className="text-info">Subtotal: $999</span>
+                        <div className="card-actions">
+                          <button className="btn btn-primary btn-block">
+                            View cart
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="dropdown dropdown-end">
-                  <label
-                    tabIndex={0}
-                    className="btn btn-ghost btn-circle avatar"
-                  >
-                    <div className="w-10 rounded-full">
-                      <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                    </div>
-                  </label>
-                  <ul
-                    tabIndex={0}
-                    className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-                  >
-                    <li>
-                      <a className="justify-between">
-                        Profile
-                        <span className="badge">New</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a>Settings</a>
-                    </li>
-                    <li>
-                      <a>Logout</a>
-                    </li>
-                  </ul>
+                  <div className="dropdown dropdown-end">
+                    <label
+                      tabIndex={0}
+                      className="btn btn-ghost btn-circle avatar"
+                    >
+                      <div className="w-10 rounded-full">
+                        <img src="man-avatar-profile-picture-vector-illustration_268834-538.png" />
+                      </div>
+                    </label>
+                    <ul
+                      tabIndex={0}
+                      className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                    >
+                      <li>
+                        <a className="justify-between">
+                          Profile
+                          <span className="badge">New</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a>Settings</a>
+                      </li>
+                      <li>
+                        <a>Logout</a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
@@ -86,9 +96,9 @@ function App() {
           <div className="drawer-side">
             <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
             <div className="h-16 px-4 flex items-center">
-              <a className="normal-case text-xl p-4">daisyUI</a>
+              <a className="normal-case text-xl p-4">Tracer</a>
             </div>
-            <ul className="menu px-4 w-80 min-h-full bg-base-200 text-base-content">
+            <ul className="menu px-4 w-80 min-h-full text-base-content">
               <li>
                 <a>Sidebar Item 1</a>
               </li>
