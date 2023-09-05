e5f1a14eb434de9f087c7aa06242a7729bc55c36
miguel.tinembart <miguel.tinembart@rebsamen.net>
2023-09-05 15:18:15 +0200

    feat: merged routes to root router

--- server/utils/trpc/routers.ts
+++ server/utils/trpc/routers.ts
@@ -2,11 +2,13 @@ import { router } from 'utils/trpc/trpc';
 import { sitesRouter } from '../../modules/TRPC/siteRouters';
 import { siteGroupsRouter } from '@server/modules/TRPC/siteGroupRouters';
 import { tenantsRouter } from '@server/modules/TRPC/tenantRouter';
+import { tenantGroupsRouter } from '@server/modules/TRPC/tenantGroupRouter';
 
 export const appRouter = router({
   sites: sitesRouter,
   siteGroups: siteGroupsRouter,
   tenants: tenantsRouter,
+  tenantGroups: tenantGroupsRouter,
 });
 
 export type AppRouter = typeof appRouter;
