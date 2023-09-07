a66172305957b95d04718b70f6b8bf61e10342f0
miguel.tinembart <miguel.tinembart@gmail.com>
2023-09-07 21:58:42 +0200

    refactor: fixed import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { trpc } from "./lib/trpc";
+import { trpc } from "./trpc";
 import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
 import { Index } from "./pages";
