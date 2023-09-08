4c47123b2f21a6b26338bc86fb907b52ecab6b02
miguel.tinembart <miguel.tinembart@gmail.com>
2023-09-09 00:01:43 +0200

    fix: removed import ?: removed isDev?

--- server/utils/trpc/trpc.ts
+++ server/utils/trpc/trpc.ts
@@ -1,12 +1,10 @@
 import { initTRPC } from '@trpc/server';
 import { Context } from './context';
-import superjson from 'superjson';
 
 const t = initTRPC.context<Context>().create({
   errorFormatter({ shape }) {
     return shape;
   },
-  isDev: true,
 });
 
 export const router = t.router;
