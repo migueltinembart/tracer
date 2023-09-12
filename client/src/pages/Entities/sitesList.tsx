f4b88a16c153889f6edc622a6706f465bf73104d
miguel.tinembart <miguel.tinembart@gmail.com>
2023-09-12 11:27:45 +0200

    merged table.tsx into sitetable and will be used for future tables

--- client/src/pages/Entities/sitesList.tsx
+++ client/src/pages/Entities/sitesList.tsx
@@ -1,6 +1,4 @@
 import { trpc } from "@/trpc";
-import { SitesForm } from "../../components/layout/table";
-import { ColumnDef } from "@tanstack/react-table";
 import { RouterOutput } from "@/trpc";
 import { Checkbox } from "@/components/ui/checkbox";
 import { CaretSortIcon, DotsHorizontalIcon } from "@radix-ui/react-icons";
@@ -10,121 +8,353 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
+  DropdownMenuCheckboxItem,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import moment from "moment";
+import {
+  AlertDialogTrigger,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useState } from "react";
+
+import {
+  DialogTitle,
+  DialogContent,
+  DialogTrigger,
+  DialogHeader,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { CreateButton } from "@/components/layout/navbar/createButton";
+import { SiteForm } from "@/components/form/createSiteForm";
+import { Link } from "react-router-dom";
 
 export type SiteOutput = RouterOutput["sites"]["select"]["one"];
 
-export const columns: ColumnDef<SiteOutput>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={table.getIsAllPageRowsSelected()}
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    id: "name",
-    accessorFn: (row) => row.name,
-    header: ({ column }) => {
-      return (
-        <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Name
-          <CaretSortIcon className="ml-2 h-4 w-4" />
-        </Button>
-      );
-    },
-  },
-  {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({ row }) => (
-      <div className="lowercase">{row.getValue("status")}</div>
-    ),
-  },
-  {
-    id: "SiteGroup",
-    accessorKey: "siteGroup",
-    header: () => <div>Site Group</div>,
-    cell: ({ row }) => (
-      <div className="lowercase">{row?.original.siteGroup?.name}</div>
-    ),
-  },
-  {
-    id: "createdAt",
-    accessorFn: (row) => row.createdAt,
-    enableHiding: true,
-    header: () => <div>Created at</div>,
-    cell: ({ row }) => (
-      <div>{moment(row.original.createdAt).format("DD.MM.YYYY")}</div>
-    ),
-  },
-  {
-    id: "updatedAt",
-    accessorFn: (row) => row.updatedAt,
-    enableHiding: true,
-    header: () => <div>Updated at</div>,
-    cell: ({ row }) => (
-      <div>{moment(row.original.updatedAt).format("DD.MM.YYYY")}</div>
-    ),
-  },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const item = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>Delete</DropdownMenuItem>
-            <DropdownMenuItem>edit</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
-];
-
 export function SitesList() {
-  const query = trpc.sites.select.all.useQuery();
+  const [deleteItem, setDeleteItem] = useState<number>(0);
+  const siteDeleter = trpc.sites.delete.one.useMutation();
+  const siteQuery = trpc.sites.select.all.useQuery();
+  const utils = trpc.useContext();
 
-  if (query.isLoading) {
-    <div>is Loading</div>;
-  }
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
-  if (query.isSuccess) {
-    return (
-      <SitesForm
-        data={query.data}
-        columns={columns}
-        initialVisibilityState={{ createdAt: false, updatedAt: false }}
-      ></SitesForm>
-    );
-  }
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({
+    updatedAt: true,
+    createdAt: true,
+  });
+  const [rowSelection, setRowSelection] = useState({});
+
+  const columns: ColumnDef<SiteOutput>[] = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={table.getIsAllPageRowsSelected()}
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      id: "name",
+      accessorFn: (row) => row.name,
+      cell: ({ row }) => (
+        <Link to={`/sites/${row.original.id}`}>
+          <Button variant={"link"}>{row.original.name}</Button>
+        </Link>
+      ),
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Name
+            <CaretSortIcon className="ml-2 h-4 w-4" />
+          </Button>
+        );
+      },
+    },
+    {
+      id: "status",
+      accessorFn: (row) => row.status,
+      header: "Status",
+      cell: ({ row }) => (
+        <div className="lowercase">{row.getValue("status")}</div>
+      ),
+    },
+    {
+      id: "SiteGroup",
+      accessorFn: (row) => row.siteGroup?.name,
+      header: () => <div>Site Group</div>,
+      cell: ({ row }) => (
+        <div className="lowercase">{row?.original.siteGroup?.name}</div>
+      ),
+    },
+    {
+      id: "createdAt",
+      accessorFn: (row) => row.createdAt,
+      enableHiding: true,
+      header: () => <div>Created at</div>,
+      cell: ({ row }) => (
+        <div>{moment(row.original.createdAt).format("DD.MM.YYYY")}</div>
+      ),
+    },
+    {
+      id: "updatedAt",
+      accessorFn: (row) => row.updatedAt,
+      enableHiding: true,
+      header: () => <div>Updated at</div>,
+      cell: ({ row }) => (
+        <div>{moment(row.original.updatedAt).format("DD.MM.YYYY")}</div>
+      ),
+    },
+    {
+      id: "actions",
+      enableHiding: false,
+      cell: ({ row }) => {
+        return (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <DotsHorizontalIcon className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+
+              <AlertDialogTrigger asChild>
+                <DropdownMenuItem
+                  className="text-red-600"
+                  onClick={() => setDeleteItem(row.original.id)}
+                >
+                  Delete
+                </DropdownMenuItem>
+              </AlertDialogTrigger>
+
+              <DropdownMenuItem>edit</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        );
+      },
+    },
+  ];
+
+  const table = useReactTable({
+    data: siteQuery.data ?? [],
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    initialState: {
+      pagination: {
+        pageSize: 25,
+        pageIndex: 0,
+      },
+    },
+    state: {
+      sorting,
+      columnFilters,
+      rowSelection,
+      columnVisibility,
+    },
+  });
+
+  return (
+    <>
+      <div className="border max-md:border-hidden flex-1 border-gray-300 rounded-lg p-6 max-md:p-0 h-full">
+        <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-3xl">
+          Sites
+        </h1>
+        <div className="w-full">
+          <div className="flex items-center justify-between py-4">
+            <Input
+              placeholder="Filter by Site..."
+              value={
+                (table.getColumn("name")?.getFilterValue() as string) ?? ""
+              }
+              onChange={(event) =>
+                table.getColumn("name")?.setFilterValue(event.target.value)
+              }
+              className="max-w-sm"
+            />
+            <div className="flex gap-2">
+              <CreateButton
+                goToElement={<SiteForm />}
+                title="Create Site"
+              ></CreateButton>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="outline">
+                    Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  {table
+                    .getAllColumns()
+                    .filter((column) => column.getCanHide())
+                    .map((column) => {
+                      return (
+                        <DropdownMenuCheckboxItem
+                          key={column.id}
+                          className="capitalize"
+                          checked={column.getIsVisible()}
+                          onCheckedChange={(value) =>
+                            column.toggleVisibility(!!value)
+                          }
+                        >
+                          {column.id}
+                        </DropdownMenuCheckboxItem>
+                      );
+                    })}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </div>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <TableHead key={header.id}>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                        </TableHead>
+                      );
+                    })}
+                  </TableRow>
+                ))}
+              </TableHeader>
+              <TableBody>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      data-state={row.getIsSelected() && "selected"}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell
+                      colSpan={columns.length}
+                      className="h-24 text-center"
+                    >
+                      No results.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </div>
+          <div className="flex items-center justify-end space-x-2 py-4">
+            <div className="flex-1 text-sm text-muted-foreground">
+              {table.getFilteredSelectedRowModel().rows.length} of{" "}
+              {table.getFilteredRowModel().rows.length} row(s) selected.
+            </div>
+            <div className="space-x-2">
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.previousPage()}
+                disabled={!table.getCanPreviousPage()}
+              >
+                Previous
+              </Button>
+              <Button
+                variant="outline"
+                size="sm"
+                onClick={() => table.nextPage()}
+                disabled={!table.getCanNextPage()}
+              >
+                Next
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              siteDeleter.mutate(deleteItem);
+              utils.invalidate();
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </>
+  );
 }
