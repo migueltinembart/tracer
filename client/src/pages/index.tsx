ee9bb517d613ee66a036237012d9a76441455e07
miguel.tinembart <miguel.tinembart@gmail.com>
2023-09-12 11:26:53 +0200

    feat: encased outlet with Dialogs and AlertDialogs so underlying elements dont have to place Dialog inside of root of document

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { NavBar } from "@/components/layout/navbar/navbar";
+import { AlertDialog } from "@/components/ui/alert-dialog";
 import { Toaster } from "@/components/ui/toaster";
+import { Dialog } from "@radix-ui/react-dialog";
 import { Outlet } from "react-router-dom";
 
 export function Index() {
@@ -8,7 +10,11 @@ export function Index() {
       <div className="flex flex-1 flex-col pb-6">
         <NavBar className="h-12  flex items-center gap-8 py-2  lg:px-28 max-lg:px-6"></NavBar>
         <div className="w-full flex flex-col flex-1 lg:px-28 max-lg:px-6">
-          <Outlet ></Outlet>
+          <AlertDialog>
+            <Dialog>
+              <Outlet></Outlet>
+            </Dialog>
+          </AlertDialog>
         </div>
       </div>
       <Toaster></Toaster>
