7624179b7766917fc28e35c4ba8da67a96fc4159
miguel.tinembart <miguel.tinembart@gmail.com>
2023-09-19 15:20:38 +0200

    imported db from feature branch

---
+++ server/db/IPAM.ts
@@ -0,0 +1,103 @@
+import { pgEnum, pgTable, primaryKey, text, timestamp, uniqueIndex, serial, integer, uuid } from 'drizzle-orm/pg-core';
+import { devices } from './deviceManagement';
+import { sites, tenants } from './entities';
+
+export const ipAdressRangeRolesEnum = pgEnum('ip_adress_range_enum', ['dhcp', 'reserved', 'active', 'inactive']);
+
+// single ip adresses only
+export const ipAdresses = pgTable(
+  'ip_adresses',
+  {
+    id: uuid('id').notNull(),
+    adress: text('adress').notNull(),
+    deviceId: uuid('device_id').references(() => devices.id),
+    dnsName: text('dns_name'),
+    description: text('description'),
+    tenantId: integer('tenant_id').references(() => tenants.id),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (ipAdresses) => {
+    return {
+      cpk: primaryKey(ipAdresses.id),
+    };
+  }
+);
+
+// ranges of ip ad
+export const ipAdressRanges = pgTable(
+  'ip_adress_ranges',
+  {
+    id: serial('id').notNull(),
+    name: text('name'),
+    description: text('description'),
+    startadress: text('start_adress').notNull(),
+    endadress: text('end_adress').notNull(),
+    status: ipAdressRangeRolesEnum('status'),
+    roleId: integer('role_id').references(() => networkRoles.id),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (adressRanges) => {
+    return {
+      cpk: primaryKey(adressRanges.id),
+      nameIndex: uniqueIndex('ip_adress_ranges_name_index').on(adressRanges.name),
+    };
+  }
+);
+
+export const subnets = pgTable(
+  'subnets',
+  {
+    id: serial('id').notNull(),
+    prefix: text('prefix').notNull(),
+    description: text('description'),
+    roleId: integer('role_id').references(() => networkRoles.id),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (subnets) => {
+    return {
+      cpk: primaryKey(subnets.id),
+      prefixIndex: uniqueIndex('subnets_prefix_index').on(subnets.prefix),
+    };
+  }
+);
+
+export const vlans = pgTable(
+  'vlans',
+  {
+    id: serial('id').notNull(),
+    name: text('name').notNull(),
+    description: text('description'),
+    vlanId: integer('vlan_id').notNull(),
+    siteId: integer('site_id').references(() => sites.id),
+    roleId: integer('role_id').references(() => networkRoles.id),
+    subnetId: integer('subnet_id').references(() => subnets.id),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (vlans) => {
+    return {
+      cpk: primaryKey(vlans.id),
+      nameIndex: uniqueIndex('vlans_name_index').on(vlans.name),
+    };
+  }
+);
+
+export const networkRoles = pgTable(
+  'network_roles',
+  {
+    id: serial('id').notNull(),
+    name: text('name').notNull(),
+    description: text('description'),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (roles) => {
+    return {
+      cpk: primaryKey(roles.id),
+      nameIndex: uniqueIndex('network_roles_name_index').on(roles.name),
+    };
+  }
+);
---
+++ server/db/deviceManagement.ts
@@ -0,0 +1,196 @@
+import { relations, sql } from 'drizzle-orm';
+import {
+  pgEnum,
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+  uniqueIndex,
+  serial,
+  integer,
+  uuid,
+  foreignKey,
+} from 'drizzle-orm/pg-core';
+import { sites, racks } from './entities';
+import { colors } from './configurations';
+
+export const protocolEnum = pgEnum('procotolEnum', ['tpc', 'udp']);
+
+export const devices = pgTable(
+  'devices',
+  {
+    id: uuid('id').notNull().defaultRandom(),
+    name: text('name').notNull(),
+    siteId: integer('site_id')
+      .notNull()
+      .references(() => sites.id),
+    rackId: uuid('rack_id').references(() => racks.id),
+    deviceTypeId: integer('device_type_id')
+      .notNull()
+      .references(() => deviceTypes.id),
+    plattformId: integer('plattform_id').references(() => plattforms.id),
+    height: integer('height'),
+    frontImage: text('front_image'),
+    backImage: text('back_image'),
+    position: integer('position'),
+    description: text('description'),
+    serialNumber: text('serial_number'),
+    color: text('color').references(() => colors.name),
+    qrCodeId: uuid('qr_code_id'), // Reference defined inside deviceToQrCodeRelations
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (devices) => {
+    return {
+      cpk: primaryKey(devices.id),
+      nameIndex: uniqueIndex('devices_name_index').on(devices.name),
+    };
+  }
+);
+
+export const devicesToDeviceRoles = pgTable('devicesToDeviceRoles', {
+  deviceId: uuid('device_id')
+    .notNull()
+    .references(() => devices.id),
+  deviceRoleId: integer('device_role_id')
+    .notNull()
+    .references(() => deviceRoles.id),
+});
+
+export const deviceTypes = pgTable(
+  'device_types',
+  {
+    id: serial('id'),
+    name: text('name').notNull(),
+    description: text('description'),
+    manufacturer: integer('manufacturer').references(() => manufacturer.id),
+    height: integer('height'),
+    frontImage: text('front_image'),
+    backImage: text('back_image'),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (deviceTypes) => {
+    return {
+      cpk: primaryKey(deviceTypes.id),
+      idIndex: uniqueIndex('device_types_id_index').on(deviceTypes.id),
+      nameIndex: uniqueIndex('device_types_name_index').on(deviceTypes.name),
+    };
+  }
+);
+
+export const deviceRoles = pgTable(
+  'device_roles',
+  {
+    id: serial('id').notNull(),
+    name: text('name').notNull(),
+    color: text('name')
+      .notNull()
+      .references(() => colors.name),
+    description: text('description'),
+  },
+  (deviceRoles) => {
+    return {
+      cpk: primaryKey(deviceRoles.id),
+      idIndex: uniqueIndex('device_roles_id_index').on(deviceRoles.id),
+    };
+  }
+);
+
+export const manufacturer = pgTable(
+  'manufacturer',
+  {
+    id: serial('id').notNull(),
+    name: text('name').notNull(),
+    description: text('description'),
+  },
+  (manufacturer) => {
+    return {
+      cpk: primaryKey(manufacturer.id),
+      idIndex: uniqueIndex('manufacturer_id_index').on(manufacturer.id),
+    };
+  }
+);
+
+export const plattforms = pgTable(
+  'plattforms',
+  {
+    id: serial('id').notNull(),
+    name: text('name').notNull(),
+    description: text('description'),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (plattforms) => {
+    return {
+      cpk: primaryKey(plattforms.id),
+      idIndex: uniqueIndex('plattforms_id_index').on(plattforms.id),
+    };
+  }
+);
+
+export const qrCodes = pgTable(
+  'qr_codes',
+  {
+    id: serial('id').notNull(),
+    deviceId: uuid('device_id'),
+    value: text('value').notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (qrCodes) => {
+    return {
+      cpk: primaryKey(qrCodes.id),
+      idIndex: uniqueIndex('qr_codes_id_index').on(qrCodes.id),
+    };
+  }
+);
+
+export const deviceToQrCodeRelations = relations(qrCodes, ({ one }) => ({
+  uniqueQrCode: one(devices, {
+    fields: [qrCodes.deviceId],
+    references: [devices.id],
+  }),
+}));
+
+export const interfaces = pgTable(
+  'interfaces',
+  {
+    id: uuid('id').notNull().defaultRandom(),
+    name: text('name').notNull(),
+    label: text('label'),
+    deviceId: uuid('device_id')
+      .notNull()
+      .references(() => devices.id),
+    bridgeId: uuid('bridge_id').unique(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (interfaces) => {
+    return {
+      cpk: primaryKey(interfaces.id),
+      reference: foreignKey({
+        columns: [interfaces.bridgeId],
+        foreignColumns: [interfaces.id],
+      }),
+    };
+  }
+);
+
+export const services = pgTable(
+  'services',
+  {
+    id: uuid('id').notNull(),
+    name: text('name').notNull(),
+    deviceId: uuid('device_id').notNull(),
+    protocol: protocolEnum('protocol'),
+    port: integer('port'),
+    description: text('description'),
+  },
+  (services) => {
+    return {
+      cpk: primaryKey(services.id),
+      nameIndex: uniqueIndex('services_name_index').on(services.name),
+    };
+  }
+);
