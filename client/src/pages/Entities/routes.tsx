c0f83a939b935e6e355d37525be7c89c777b40ad
miguel.tinembart <miguel.tinembart@gmail.com>
2023-09-19 10:35:05 +0200

    feat: added tenantGroups as route option

--- client/src/pages/Entities/routes.tsx
+++ client/src/pages/Entities/routes.tsx
@@ -3,6 +3,7 @@ import { Sites } from "./Sites/sites";
 import { SiteGroups } from "./SiteGroups/siteGroups";
 import { Tenants } from "./Tenants/tenants";
 import { Locations } from "./Locations/locations";
+import { TenantGroups } from "./TenantGroups/TenantGroups";
 
 export const entityRoutes: RouteObject = {
   path: "entities",
@@ -21,6 +22,7 @@ export const entityRoutes: RouteObject = {
     },
     {
       path: "tenant-groups",
+      element: <TenantGroups />,
     },
     {
       path: "locations",
