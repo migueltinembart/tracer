9f474d38d5e8fe3fa840bb4bff004b0fe7419fd9
miguel.tinembart <miguel.tinembart@gmail.com>
2023-09-20 11:29:29 +0200

    test: trpc connection tested with test page setup

--- server/appRouter.ts
+++ server/appRouter.ts
@@ -1,5 +1,11 @@
-import { router, publicProcedure } from "./trpc";
+import { router } from "./trpc";
+import { sitesRouter } from "./routers/entities/siteRouter";
+import { tenantsRouter } from "./routers/entities/tenantRouter";
+import { z } from "zod";
 
-const appRouter = router({
+export const appRouter = router({
   sites: sitesRouter,
+  tenants: tenantsRouter,
 });
+
+export type AppRouter = typeof appRouter;
