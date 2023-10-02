3b86f304b33e96fa0791e8ff71365402618cfd9c
miguel.tinembart <miguel.tinembart@gmail.com>
2023-10-02 14:36:32 +0200

    index page created
    - Base Grid layout implemented
    - Cards get a new design

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,67 @@
-"use client";
-import { trpc } from "./_trpc/client";
-import { useSession } from "next-auth/react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import clsx from "clsx";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
-  const session = useSession();
-  const { data } = trpc.test.useQuery();
+const cardClasses = clsx(["overflow-hidden"]);
 
-  return <div>{}</div>;
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  return (
+    <div className=" p-6 flex-1 flex flex-col border lg:shadow-sm rounded-xl max-md:border-hidden">
+      <div className="text-xl p-3 pb-8">
+        <p>Welcome, </p>
+        <p className="text-3xl font-bold">{session?.user.name}</p>
+      </div>
+
+      <div className="grid min-h-min gap-6 flex-1 grid-cols-6 grid-rows-4">
+        <Card
+          className={clsx(cardClasses, ["col-span-4 row-start-1 row-span-1"])}
+        >
+          <CardHeader className="pb-2 border-b bg-gray-50">
+            <CardTitle>Projects</CardTitle>
+            <CardDescription>
+              Jump right in to your last projects
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="grid items-stretch flex-1 grid-cols-6 grid-rows-1 gap-3 pt-6"></CardContent>
+        </Card>
+
+        <Card
+          className={clsx(cardClasses, ["row-start-2 col-span-4 row-span-3"])}
+        >
+          <CardHeader className="pb-2 border-b bg-gray-50">
+            <CardTitle>Dashboard</CardTitle>
+            <CardDescription>
+              All your favorite menu items in one place
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="grid items-stretch flex-1 grid-cols-6 grid-rows-1 gap-3 pt-6"></CardContent>
+        </Card>
+        <Card
+          className={clsx(cardClasses, [
+            "col-span-2 row-span-full col-start-5",
+          ])}
+        >
+          <CardHeader className="pb-2 border-b bg-gray-50">
+            <CardTitle>News Feed</CardTitle>
+            <CardDescription>
+              See what others have done in your organization
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent className="grid items-stretch flex-1 grid-cols-6 grid-rows-1 gap-3 pt-6"></CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
