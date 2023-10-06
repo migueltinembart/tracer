585cec814cf8f66ea358644ef3e37e82f2912415
miguel.tinembart <miguel.tinembart@gmail.com>
2023-10-06 11:37:30 +0200

    feat: Navbar generated through array of listelements
    - fix: Profile Menu styled
    - add: Bade added from shadcn

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -1,14 +1,129 @@
 import Link from "next/link";
 import { ReactNode } from "react";
-import { NavMenuSmall } from "./NavMenus";
+import { MenubarItem, NavMenuLarge, NavMenuSmall } from "./NavMenus";
 import clsx from "clsx";
 import { sharedPadding } from "./Shared";
-import { AvatarIcon } from "./Avatar";
+import { ProfileMenu } from "./Profile";
 import { getServerSession } from "next-auth";
-import { useSession } from "next-auth/react";
+
+import { NavMenuSearchBar } from "./Search";
+import { Component1Icon, CubeIcon, RocketIcon } from "@radix-ui/react-icons";
+import { Separator } from "@/components/ui/separator";
+
+const iconSize = 15;
+
+const list: MenubarItem[] = [
+  {
+    title: "Entities",
+    image: <CubeIcon height={iconSize} width={iconSize} />,
+    items: [
+      {
+        name: "Tenants",
+        description: "description",
+        listItems: [
+          {
+            label: "Tenants",
+            href: "entities/tenants",
+          },
+          {
+            label: "Tenant Groups",
+            href: "entities/tenant-groups",
+          },
+        ],
+      },
+      {
+        name: "Sites",
+        description: "Description",
+        listItems: [
+          {
+            label: "Sites",
+            href: "/entities/sites",
+          },
+          {
+            label: "Site Groups",
+            href: "/entities/site-groups",
+          },
+        ],
+      },
+      {
+        name: "Placement",
+        description: "Description",
+        listItems: [
+          { label: "Locations", href: "entities/locations" },
+          {
+            label: "Racks",
+            href: "entities/racks",
+          },
+          {
+            label: "Rack Roles",
+            href: "entities/rack-roles",
+          },
+        ],
+      },
+      {
+        name: "Contacts",
+        description: "Description",
+        listItems: [
+          {
+            label: "Contacts",
+            href: "entities/contacts",
+          },
+          {
+            label: "Contact Groups",
+            href: "entities/contact-groups",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Device Management",
+    image: <Component1Icon height={iconSize} width={iconSize} />,
+    items: [
+      {
+        name: "Devices",
+        description: "Description",
+        listItems: [
+          { label: "Devices", href: "device-management/devices" },
+          { label: "Device Types", href: "device-management/device-types" },
+          { label: "Device Roles", href: "device-management/device-roles" },
+          { label: "Manufacturers", href: "device-management/manufacturers" },
+          { label: "Plattforms", href: "device-management/plattforms" },
+          { label: "QR-Codes", href: "device-management/qr-codes" },
+          { label: "Interfaces", href: "device-management/interfaces" },
+          { label: "Services", href: "device-management/services" },
+        ],
+      },
+      {
+        name: "IPAM",
+        description: "Description",
+        listItems: [
+          { label: "IP Adresses", href: "ipam/ip-adresses" },
+          { label: "IP Adress Ranges", href: "ipam/ip-address-ranges" },
+          { label: "Subnets", href: "ipam/subnets" },
+          { label: "Vlans", href: "ipam/vlans" },
+          { label: "Network Roles", href: "ipam/network-roles" },
+        ],
+      },
+      {
+        name: "Wireless",
+        description: "Description",
+        listItems: [
+          { label: "Wireless LANs", href: "wireless/wireless-lans" },
+          { label: "WLAN Groups", href: "wireless/wireless-lan-groups" },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Project Management",
+    image: <RocketIcon height={iconSize} width={iconSize} />,
+    items: [],
+  },
+];
 
 const headerClass = clsx([
-  "h-14 flex justify-between items-center shadow-md",
+  "h-12 items-center grid grid-cols-3 grid-rows-1 border-b shadow-sm",
   sharedPadding,
 ]);
 
@@ -17,12 +132,24 @@ export default async function NavBar({ children }: { children?: ReactNode }) {
 
   return (
     <header className={headerClass}>
-      <div className="flex items-center">
+      <div className="flex items-center col-start-1 gap-5">
         <NavMenuSmall></NavMenuSmall>
-        <Link href={"/"}>Tracer</Link>
+
+        <Link href={"/"} className="flex items-center text-xl">
+          Tracer
+        </Link>
+        <NavMenuSearchBar className="flex items-center justify-between w-[250px] max-md:hidden" />
       </div>
-      <div className="max-sm:hidden">{children}</div>
-      <AvatarIcon imageSrc={session?.user.image}></AvatarIcon>
+
+      <NavMenuLarge
+        className="justify-center col-start-2 shadow-none border-hidden"
+        menuItems={list}
+      ></NavMenuLarge>
+      <ProfileMenu
+        name={session?.user.name}
+        email={session?.user.email}
+        image={session?.user.image}
+      ></ProfileMenu>
     </header>
   );
 }
--- app/_components/Profile.tsx
+++ app/_components/Profile.tsx
@@ -1,48 +1,64 @@
 "use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
 import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
-} from "@radix-ui/react-hover-card";
+} from "@/components/ui/hover-card";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { UserCog } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { GearIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
-export function AvatarIcon(props: { imageSrc?: string | null }) {
-  if (props.imageSrc) {
+export function AvatarIcon({ src }: { src?: string | null }) {
+  if (src) {
     return (
       <Avatar>
-        <AvatarImage src={props.imageSrc} alt="Profile Picture"></AvatarImage>
+        <AvatarImage src={src} alt="Profile Picture"></AvatarImage>
       </Avatar>
     );
   } else <div>no avatar</div>;
 }
 
 export function ProfileMenu({
+  className,
+  name,
   email,
   image,
 }: {
+  className?: string;
+  name?: string | null;
   email?: string | null;
   image?: string | null;
 }) {
   return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <Button variant={"link"}>
-          <AvatarIcon imageSrc={image}></AvatarIcon>
-        </Button>
-      </HoverCardTrigger>
-      <HoverCardContent className="pt-6">
-        <div className="p-6 mx-2 space-x-4 bg-white border rounded-lg">
-          <div>
-            
-            <p className="text-sm pb-2">{email}</p>
-            <p className="text-sm">Settings</p>
-            <Button onClick={() => signOut()}>Sign Out</Button>
+    <div className={"flex justify-end"}>
+      <HoverCard>
+        <HoverCardTrigger asChild>
+          <Button variant={"link"}>
+            <AvatarIcon src={image}></AvatarIcon>
+          </Button>
+        </HoverCardTrigger>
+        <HoverCardContent className="bg-none p-4 w-[500]">
+          <div className=" flex justify-between py-2">
+            <Badge>Role</Badge>
+            <Link href={"/user/settings"}>
+              <GearIcon></GearIcon>
+            </Link>
           </div>
-        </div>
-      </HoverCardContent>
-    </HoverCard>
+          <CardTitle>{name}</CardTitle>
+          <CardDescription>{email}</CardDescription>
+        </HoverCardContent>
+      </HoverCard>
+    </div>
   );
 }
