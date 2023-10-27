d56d3d2a7a1877e73284c7ced71390c1064772db
migueltinembart <miguel.tinembart@gmail.com>
2023-10-27 15:19:24 +0200

    add: color-picker component added

---
+++ components/ui/color-picker.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Wheel } from "@uiw/react-color";
+import { cn } from "@/lib/utils";
+import { Paintbrush } from "lucide-react";
+import Link from "next/link";
+import { useMemo, useState } from "react";
+
+export function PickerExample() {
+  const [background, setBackground] = useState("#B4D455");
+
+  return (
+    <div
+      className="w-full h-full preview flex min-h-[350px] justify-center p-10 items-center rounded !bg-cover !bg-center transition-all"
+      style={{ background }}
+    >
+      <ColorPicker background={background} setBackground={setBackground} />
+    </div>
+  );
+}
+
+export function ColorPicker({
+  background,
+  setBackground,
+  className,
+}: {
+  background: string;
+  setBackground: (background: string) => void;
+  className?: string;
+}) {
+  const solids = [
+    "#E2E2E2",
+    "#ff75c3",
+    "#ffa647",
+    "#ffe83f",
+    "#9fff5b",
+    "#70e2ff",
+    "#cd93ff",
+    "#09203f",
+    "#0099fc",
+  ];
+
+  const defaultTab = useMemo(() => {
+    return "solid";
+  }, [background]);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[220px] justify-start text-left font-normal",
+            !background && "text-muted-foreground",
+            className
+          )}
+        >
+          <div className="w-full flex items-center gap-2">
+            {background ? (
+              <div
+                className="h-4 w-4 rounded !bg-center !bg-cover transition-all"
+                style={{ background }}
+              ></div>
+            ) : (
+              <Paintbrush className="h-4 w-4" />
+            )}
+            <div className="truncate flex-1">
+              {background ? background : "Pick a color"}
+            </div>
+          </div>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-64">
+        <div className="flex flex-col">
+          <div className="pb-3 flex justify-center border rounded-sm p-2">
+            <Wheel
+              color={background}
+              onChange={(color) => setBackground(color.hex)}
+            ></Wheel>
+          </div>
+          <div className="flex flex-wrap gap-1 w-full py-3">
+            {solids.map((s) => (
+              <div
+                key={s}
+                style={{ background: s }}
+                className="rounded-md h-6 w-6 cursor-pointer active:scale-105"
+                onClick={() => setBackground(s)}
+              />
+            ))}
+          </div>
+        </div>
+
+        <Input
+          id="custom"
+          value={background}
+          className="col-span-2 h-8 mt-4"
+          onChange={(e) => setBackground(e.currentTarget.value)}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
